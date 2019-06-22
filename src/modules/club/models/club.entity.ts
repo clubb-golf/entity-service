826b9fddde5452e286968b817d7fb58b38df025a
David Fudacz <david.fudacz@tempus.com>
2019-06-22 17:33:20 -0500

    set up club and course entities

---
+++ src/modules/club/models/club.entity.ts
@@ -0,0 +1,23 @@
+import {Entity, PrimaryGeneratedColumn, Column, OneToMany} from 'typeorm';
+import { CourseEntity } from '../../course/models/course.entity';
+
+@Entity({ name: 'clubs' })
+export class ClubEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    informalName: string;
+
+    @Column()
+    type: string;
+
+    @Column()
+    establishedYear: string;
+
+    @OneToMany(type => CourseEntity, course => course.club)
+    courses: CourseEntity[];
+}
---
+++ src/modules/course/models/course.entity.ts
@@ -0,0 +1,20 @@
+import {Entity, PrimaryGeneratedColumn, Column, ManyToOne} from 'typeorm';
+import { ClubEntity } from '../../club/models/club.entity';
+
+@Entity({ name: 'courses' })
+export class CourseEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    informalName: string;
+
+    @Column()
+    establishedYear: string;
+
+    @ManyToOne(type => ClubEntity, club => club.courses)
+    club: ClubEntity;
+}
